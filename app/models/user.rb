43b615e3e65ae7527c447693093ad246c4ef1cc2
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-22 10:30:40 -0600

    added user model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  def self.find_or_create_from_auth(data)
+    user = User.find_or_create_by(provider: data.provider, uid: data.uid)
+
+    user.name      = data.info.name
+    user.email     = data.info.email
+    user.token     = data.credentials.token
+    user.image_url = data.info.image
+    user.save
+    user
+  end
+end
