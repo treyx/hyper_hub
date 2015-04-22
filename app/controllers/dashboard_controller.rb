d4487496b6dda3148bc29d58cfa96170422d7cb4
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-22 10:32:03 -0600

    authorization

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
+  before_action :authorize!
+
   def show
+  end
+
+  private
+
+  def authorize!
+    redirect_to root_path unless current_user
   end
 end
