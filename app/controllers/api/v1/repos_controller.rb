57d1b4ad3cd9d0b1a32a09749a0208e1bda23802
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-29 09:04:49 -0600

    still a wip

--- app/controllers/api/v1/repos_controller.rb
+++ app/controllers/api/v1/repos_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ReposController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Repo.public_repos
+    respond_with Repo.parse_repo(params[:owner], params[:repo])
   end
+
 end
