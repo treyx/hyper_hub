625a27690cf362cc0dfbf1347066f5250e9beb2f
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-29 10:08:47 -0600

    cleaned up api service

--- app/services/repo_service.rb
+++ app/services/repo_service.rb
@@ -1,25 +1,15 @@
 class RepoService
-
   attr_reader :connection
 
   def initialize
-    @connection = Faraday.new(url: "https://api.github.com/repos/treyx/hyperhub")
+    @connection = Faraday.new(url: "https://api.github.com/repos")
   end
 
-  def self.find_repo
-    JSON.parse(connection.get("schools").body, symbolize_names: true)
+  def self.find_repo(owner, repo)
+    parse(connection.get("#{owner}/#{repo}").body, symbolize_names: true)
   end
 
-
-
-
-  def self.parse_repo
-    repository = find_repo
-    JSON.parse(repository.body, symbolize_names: true)
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
-
-  # def self.repo_contributors(owner, repo)
-  #   Faraday.get "https://api.github.com/repos/#{owner}/#{repo}/stats/contributors"
-  # end
-
 end
