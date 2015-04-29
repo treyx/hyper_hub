24f354cf330c893fb91d4dc22d2ed5730d15de05
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-29 11:51:55 -0600

    cleaned up angular controller

--- app/assets/javascripts/angular/controllers/repos_ctrl.js
+++ app/assets/javascripts/angular/controllers/repos_ctrl.js
@@ -1,6 +1,4 @@
 app.controller('ReposCtrl', ['$scope', '$resource', function($scope, $resource) {
   var Repos = $resource('/api/repos');
   $scope.repos = Repos.query();
-  $scope.options = Repos.query();
-  $scope.graph = Repos.query();
 }]);
