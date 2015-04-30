a4029b2533ebb19756c4128b8bf29576c6d929ae
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-29 22:25:37 -0600

    modified angular service - still a work in progress

---
+++ app/assets/javascripts/angular/services/repo_service.js
@@ -0,0 +1,17 @@
+app.service('Repo', ['$resource', function($resource) {
+    this.getRepoData = function(owner, repo){
+    var repo = $resource('/api/v1/repos', {owner: "@owner", repo: "@repo"});
+    .success(function (data){
+
+    })
+    .error(function (data, status) {
+      if (status === 404) {
+        $scope.error = 'That repository does not exist';
+      } else {
+        $scope.error = 'Error: ' + status;
+      }
+    });
+
+    return repo.query(owner: owner, repo: repo )
+
+}]);
