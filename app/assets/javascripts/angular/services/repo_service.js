58ec7dd787ae47751507cf78baddf972d6ccf61a
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-02 21:13:12 -0600

    using app.service now - this creates a singleton object for the entire application

--- app/assets/javascripts/angular/services/repo_service.js
+++ app/assets/javascripts/angular/services/repo_service.js
@@ -1,17 +1,9 @@
-// app.service('Repo', ['$resource', function($resource) {
-//     this.getRepoData = function(owner, repo){
-//     var repo = $resource('/api/v1/repos', {owner: "@owner", repo: "@repo"});
-//     .success(function (data){
+app.service("RepoSearch", function($resource){
 
-//     })
-//     .error(function (data, status) {
-//       if (status === 404) {
-//         $scope.error = 'That repository does not exist';
-//       } else {
-//         $scope.error = 'Error: ' + status;
-//       }
-//     });
+  var RepoSearch = $resource("/api/v1/repos", {user: "@searchUser", repo: "@searchRepo"});
 
-//     return repo.query(owner: owner, repo: repo )
+  this.search = function(){
+      return RepoSearch.query();
+  }
 
-// }]);
+});
