ec3f1808c131bc44bcb582be6c15865ab5bc20a2
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-05 00:52:10 -0600

    fixed controller spec

--- spec/controllers/api/v1/repos_controller_spec.rb
+++ spec/controllers/api/v1/repos_controller_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::ReposController, type: :controller do
-
   it "responds to #index" do
-    get :index, format: :json, user: "treyx", repo: "hyperhub"
-    expect(response.status).to eq(200)
-    repo = JSON.parse(response.body, symbolize_names: true)
-    expect(repo[:name]).to eq("hyperhub")
-    expect(repo[:owner][:login]).to eq("treyx")
+    VCR.use_cassette 'index_for_treyx_hyperhub' do
+      get :index, format: :json, user: "treyx", repo: "hyperhub"
+      expect(response.status).to eq(200)
+      repo = JSON.parse(response.body, symbolize_names: true)
+      expect(repo[:name]).to eq("hyperhub")
+      expect(repo[:owner][:login]).to eq("treyx")
+    end
   end
-
 end
