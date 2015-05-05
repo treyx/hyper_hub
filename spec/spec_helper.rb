aee71d61554f9889bbfcbb12c54dae595243d433
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-04 19:18:26 -0600

    configured spec helper for VCR integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 require "simplecov"
 SimpleCov.start
 
+
+require "vcr"
+VCR.configure do |c|
+    c.hook_into :faraday
+    c.cassette_library_dir = 'spec/support/vcr_cassettes'
+    c.default_cassette_options = {:record => :new_episodes}
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
