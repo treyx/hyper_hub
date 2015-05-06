c1ceb1b24e857fac1682ff026ed7ae56284658ac
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-06 16:31:57 -0600

    changed name of service to be more general in order to prepare for additional api calls

--- app/assets/javascripts/angular/services/repo_service.js
+++ app/assets/javascripts/angular/services/repo_service.js
@@ -1,6 +1,7 @@
-app.service("RepoSearch", function($http){
- var url= "/api/v1/repos"
- this.search = function(repo,user){
-     return $http.get(url+"?user="+user+"&repo="+repo);
- }
+app.service("GitSearch", function($http){
+ var urlRepo= "/api/v1/repos"
+ this.searchRepo = function(user,repo){
+     return $http.get(urlRepo+"?user="+user+"&repo="+repo);
+ };
+
 });
