4ecb8b9e1fe926612c963d4d9d1c6a1920e56d6b
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-07 09:10:22 -0600

    added logic for parsing commit history - everything works!

--- app/assets/javascripts/angular/controllers/repos_ctrl.js
+++ app/assets/javascripts/angular/controllers/repos_ctrl.js
@@ -4,42 +4,87 @@ app.controller("ReposCtrl", ["GitSearch", "$scope", function(GitSearch, $scope) 
   $scope.size = [];
   $scope.stars = [];
   $scope.forks = [];
+  $scope.commitHistory = [];
 
-  $scope.searchRepo = function(user, repo) {
-    GitSearch.searchRepo(user, repo).success(function(data){
+  $scope.searchRepo = function(user,repo) {
+    GitSearch.searchRepo(user,repo).success(function(data){
 
-        var newByte = convertBytes(data.size)
+      var newByte = convertBytes(data.size)
 
-        $scope.forks.push({key: data.name, y: data.forks});
-        $scope.issues.push({key: data.name, y: data.open_issues});
-        $scope.size.push({key: data.name, y: data.size});
-        $scope.stars.push({key: data.name, y: data.stargazers_count});
-
+      $scope.forks.push({key: data.name, y: data.forks});
+      $scope.issues.push({key: data.name, y: data.open_issues});
+      $scope.size.push({key: data.name, y: data.size});
+      $scope.stars.push({key: data.name, y: data.stargazers_count});
     });
-  }
+  };
+
+
+  $scope.searchCommits = function(user,repo) {
+    GitSearch.searchCommits(user,repo).success(function(data){
+
+      var obj = [];
+      for ( var i in data) {
+        var total = data[i].total;
+        var week = data[i].week;
+        obj.push({x: week, y: total})
+      }
+      $scope.commitHistory.push({key: repo, values: obj});
+  });
+    // var date = new Date(data[0].week * 1000);
+
+  };
 
   function convertBytes(bytes) {
-   if(bytes == 0) return '0 Byte';
-   var k = 1000;
-   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-   var i = Math.floor(Math.log(bytes) / Math.log(k));
-   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
-}
+    if(bytes == 0) return '0 Byte';
+    var k = 1000;
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    var i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+  };
 
-
-$scope.pieOptions = {
+        $scope.pieOptions = {
             chart: {
                 type: "pieChart",
-                height: 200,
-                width: 200,
+                height: 200 ,
+                // width: 200,
                 x: function(d){return d.key;},
                 y: function(d){return d.y;},
                 showLabels: true,
                 transitionDuration: 500,
                 labelThreshold: 0.01,
-                showLegend: false
+                showLegend: true
+            }
+        };
+
+        $scope.barOptions = {
+            chart: {
+                type: 'multiBarChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 60,
+                    left: 45
+                },
+                clipEdge: true,
+                staggerLabels: true,
+                transitionDuration: 500,
+                stacked: true,
+                xAxis: {
+                    axisLabel: 'Week',
+                    showMaxMin: false,
+                    tickFormat: function(d){
+                        return d3.format(',f')(d);
+                    }
+                },
+                yAxis: {
+                    axisLabel: 'Number of Commits',
+                    axisLabelDistance: 40,
+                    tickFormat: function(d){
+                        return d3.format(',.1f')(d);
+                    }
+                }
             }
         };
 
 }]);
-
